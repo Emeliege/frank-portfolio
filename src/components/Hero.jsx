0414dee4b1d4cf6d9a3aa4f0abbababb523d6c31
emeliege <ibeokoyefranklin@gmail.com>
2024-12-31 20:54:45 +0100

    master

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,10 +1,11 @@
 import dp from "../image/dp2.jpg";
 import cv from "../cv/cv.jpg";
 import { GrDocumentDownload } from "react-icons/gr";
+import { Facebook, Github, Instagram, Linkedin, Mail } from "lucide-react";
 
 const Hero = () => {
   return (
-    <div id="about" className="h-[23rem] bg-[#003329] lg:h-[26rem]">
+    <div id="#about" className="h-[26.5rem] bg-[#003329] lg:h-[30rem]">
       <div className="flex flex-row-reverse md:flex-row">
         <div>
           <img
@@ -41,6 +42,24 @@ const Hero = () => {
           Download CV <GrDocumentDownload className="text-xl sm:text-3xl"/>
         </a>
     </div>
+    <div className="flex text-[#abd1c6] text-[22px] justify-center mt-2 sm:mt-3 font-bold sm:text-[25px] lg:text-[28px]">Socials</div>
+    <div className="flex space-x-4 gap-6 justify-center sm:gap-8 lg:gap-10">
+            <a href="https://github.com/Emeliege" className="text-[#fffffe]">
+              <Github className="w-6 h-6" />
+            </a>
+            <a href="https://ng.linkedin.com/in/franklin-ibeokoye-141670238" className=" text-[#fffffe]">
+              <Linkedin className="w-6 h-6" />
+            </a>
+            <a href="mailto:ibeokoyefrankli@gmail.com.com" className=" text-[#fffffe]">
+              <Mail className="w-6 h-6" />
+              </a>
+            <a href="https://www.instagram.com/itsfrankfort_official/profilecard/?igsh=MXN4NjV4bDkwcWRhcA==" className=" text-[#fffffe]">
+              <Instagram className="w-6 h-6" />
+            </a>
+            <a href="https://www.facebook.com/share/RFg7qgSPtQRLaaWU/?mibextid=LQQJ4d" className=" text-[#fffffe]">
+              <Facebook className="w-6 h-6" />
+            </a>
+          </div>
     </div>
   );
 };
